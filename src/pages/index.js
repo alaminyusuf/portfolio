33ded438b4ad944a1861014fcd18621d85c56589
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-05-22 21:14:46 +0100

    index page refactored to use styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,23 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/Hero"
 import Nav from "../components/Navbar"
 
+const Container = styled.div`
+  width: 85%;
+  margin: 0 auto;
+`
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="container">
+    <Container>
       <Nav />
       <Hero />
-    </div>
+    </Container>
   </Layout>
 )
 
