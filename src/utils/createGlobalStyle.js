f954c9d384ec80264d4aa88c421cb49c0392a049
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-05-22 21:31:27 +0100

    created global style

---
+++ src/utils/createGlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+@import url("https://fonts.googleapis.com/css2?family=Montserrat&family=Reggae+One&display=swap");
+body {
+   font-family: "Montserrat", sans-serif;
+   font-size: 24px;
+   color: #414141;
+   background-color: #fafafa;
+ }
+ 
+`
