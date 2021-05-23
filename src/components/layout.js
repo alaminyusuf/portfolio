7d485ea4a9c27cae395b1fef91f52a8cddf2a771
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-05-23 17:57:01 +0100

    global style now available to layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,17 @@
 import React from "react"
+import styled from "styled-components"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import { GlobalStyle } from "../utils/createGlobalStyle"
 
+import Header from "./header"
+import Navbar from "../components/Navbar"
+
+const Container = styled.div`
+  width: 85%;
+  margin: 0 auto;
+`
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -18,7 +26,11 @@ const Layout = ({ children }) => {
   return (
     <React.Fragment>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <main>{children}</main>
+      <Container>
+        <GlobalStyle />
+        <Navbar />
+        <main>{children}</main>
+      </Container>
     </React.Fragment>
   )
 }
