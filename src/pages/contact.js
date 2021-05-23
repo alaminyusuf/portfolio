9fd3f65166ee1f08565460e82696c3a7cb27267e
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-05-23 18:22:24 +0100

    conatct page

---
+++ src/pages/contact.js
@@ -0,0 +1,56 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Wrapper = styled.div`
+  width: 70%;
+  margin: auto auto;
+`
+const Header = styled.h2`
+  font-family: "Reggae One", cursive;
+  font-size: 3rem;
+  font-weight: 400;
+`
+
+const Content = styled.div`
+  jusfify-content: center;
+  display: flex;
+  flex-direction: column;
+`
+
+const Email = styled(Link)`
+  text-align: center;
+  margin: 2em auto;
+  padding: 1em 5em;
+  font-size: 14px;
+  border: dotted 1px #414141;
+  outline: none;
+  text-transform: uppercase;
+  font-wieght: 400;
+  font-family: "Montserrat", sans-serif;
+  color: #414141;
+  text-decoration: none;
+
+  @media (max-width: 340px) {
+    padding: 0.5em;
+    font-size: 14px;
+  }
+`
+
+const contact = () => {
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <Wrapper>
+        <Header>Contact Me</Header>
+        <Content>
+          <Email href="mailto:alaminyusf131@gmail.com">send mail</Email>
+        </Content>
+      </Wrapper>
+    </Layout>
+  )
+}
+
+export default contact
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,15 @@
 import React from "react"
-import styled from "styled-components"
 
-import { GlobalStyle } from "../utils/createGlobalStyle"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/Hero"
-import Nav from "../components/Navbar"
 import Cta from "../components/Cta"
-
-const Container = styled.div`
-  width: 85%;
-  margin: 0 auto;
-`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <GlobalStyle />
-    <Container>
-      <Nav />
-      <Hero />
-      <Cta />
-    </Container>
+    <Hero />
+    <Cta />
   </Layout>
 )
 
